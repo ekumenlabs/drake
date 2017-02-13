e16a0a0c72d34a8067100e14028ac047f6638280
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2017-02-13 11:31:34 -0300

    Made the program simpler removing unused things.

--- drake/sdf_sample/README.md
+++ drake/sdf_sample/README.md
@@ -7,6 +7,6 @@ bazel build drake/sdf_sample:sample
 To run and see it on the visualizer:
 
 ```
-./build/install/bin/drake_visualizer &
+build/install/bin/drake-visualizer &
 ./bazel-bin/drake/sdf_sample/sample <path_to_sdf>
 ```
--- drake/sdf_sample/src/sample.cc
+++ drake/sdf_sample/src/sample.cc
@@ -15,7 +15,9 @@
 #include "drake/multibody/parsers/sdf_parser.h"
 #include "drake/multibody/parsers/urdf_parser.h"
 #include "drake/multibody/rigid_body_plant/drake_visualizer.h"
-//#include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
+#include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
+
+#include "drake/systems/controllers/pid_controlled_system.h"
 
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram.h"
@@ -25,6 +27,11 @@
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/primitives/constant_vector_source.h"
 #include "drake/systems/primitives/multiplexer.h"
+
+#include "drake/systems/primitives/constant_vector_source.h"
+#include "drake/systems/primitives/matrix_gain.h"
+
+#include "drake/common/drake_path.h"
 
 
 namespace drake {
@@ -45,8 +52,57 @@ void LoadSDFSample(const std::string &sdfFilePath) {
 
   systems::DiagramBuilder<double> builder;
 
-  builder.template AddSystem<systems::DrakeVisualizer>(*rigid_body_tree, &lcm);
-  
+  auto plant = std::make_unique<systems::RigidBodyPlant<double>>(std::move(rigid_body_tree));
+  /*const Vector3<double> Kp(100,   0,   0);  // Units: Nm / radians
+  const Vector3<double> Ki(0,     0,   0);  // Units: Nm / radians
+  const Vector3<double> Kd(100,  250, 250);  // Units: Nm / (radians / sec).
+
+  MatrixX<double> feedback_selector_matrix;
+  feedback_selector_matrix.setZero(plant->get_input_size() * 2,
+                                  plant->get_output_size());*/
+
+  // DRAKE_DEMAND(feedback_selector_matrix.rows() == 6);
+  // const int kFeedbackIndexSteeringAnglePosition = 0;
+  // const int kFeedbackIndexLeftWheelPosition     = 1;
+  // const int kFeedbackIndexRightWheelPosition    = 2;
+  // const int kFeedbackIndexSteeringAngleSpeed = 3;
+  // const int kFeedbackIndexLeftWheelSpeed     = 4;
+  // const int kFeedbackIndexRightWheelSpeed    = 5;
+
+  // DRAKE_DEMAND(feedback_selector_matrix.cols() == 27);
+  // const int kStateIndexSteeringAnglePosition = 7;
+  // const int kStateIndexLeftWheelPosition     = 9;
+  // const int kStateIndexRightWheelPosition    = 11;
+  // const int kStateIndexSteeringAngleSpeed = 20;
+  // const int kStateIndexLeftWheelSpeed     = 22;
+  // const int kStateIndexRightWheelSpeed    = 24;
+  // feedback_selector_matrix(kFeedbackIndexSteeringAnglePosition,
+  //                          kStateIndexSteeringAnglePosition) = 1;
+  // feedback_selector_matrix(kFeedbackIndexLeftWheelPosition,
+  //                          kStateIndexLeftWheelPosition) = 1;
+  // feedback_selector_matrix(kFeedbackIndexRightWheelPosition,
+  //                          kStateIndexRightWheelPosition) = 1;
+  // feedback_selector_matrix(kFeedbackIndexSteeringAngleSpeed,
+  //                          kStateIndexSteeringAngleSpeed) = 1;
+  // feedback_selector_matrix(kFeedbackIndexLeftWheelSpeed,
+  //                          kStateIndexLeftWheelSpeed) = 1;
+  // feedback_selector_matrix(kFeedbackIndexRightWheelSpeed,
+  //                          kStateIndexRightWheelSpeed) = 1;
+  /*auto feedback_selector =
+      std::make_unique<systems::MatrixGain<double>>(feedback_selector_matrix);
+
+  auto controller = builder.AddSystem<systems::PidControlledSystem>(
+      std::move(plant), std::move(feedback_selector), Kp, Ki, Kd);*/
+
+  // Instantiates a system for visualizing the model.
+  /*const RigidBodyTreed& tree_ptr =
+      dynamic_cast<const systems::RigidBodyPlant<double>*>(controller->plant())->
+          get_rigid_body_tree();*/
+  /*const RigidBodyTreed& tree_ptr =
+      dynamic_cast<const systems::RigidBodyPlant<double>*>(std::move(plant))->
+          get_rigid_body_tree();
+  auto publisher = builder.AddSystem<systems::DrakeVisualizer>(tree_ptr, &lcm);
+
   std::unique_ptr<systems::Diagram<double>> diagram = builder.Build();
 
   lcm.StartReceiveThread();
@@ -55,7 +111,39 @@ void LoadSDFSample(const std::string &sdfFilePath) {
   
   simulator.Initialize();
   
-  simulator.StepTo(std::numeric_limits<double>::infinity());
+  simulator.StepTo(std::numeric_limits<double>::infinity());*/
+}
+
+template<typename T>
+std::unique_ptr<drake::systems::RigidBodyPlant<T>>
+CreateSystem(const std::string &sdfFilePath) {
+  auto rigid_body_tree = std::make_unique<RigidBodyTree<T>>();
+  drake::parsers::sdf::AddModelInstancesFromSdfFile(sdfFilePath,
+      multibody::joints::kFixed,
+      nullptr /* weld to frame */,
+      rigid_body_tree.get());
+  return std::make_unique<drake::systems::RigidBodyPlant<T>>(
+      std::move(rigid_body_tree));
+}
+
+void LoadSDFSample2(const std::string &sdfFilePath) {
+  systems::DiagramBuilder<double> builder;
+  const systems::RigidBodyPlant<double>* rigidBodyPlant =
+      builder.AddSystem<systems::RigidBodyPlant<double>>(
+          CreateSystem<double>(sdfFilePath));
+  
+  const RigidBodyTree<double> &tree = rigidBodyPlant->get_rigid_body_tree();
+  
+  drake::lcm::DrakeLcm lcm;
+
+  const auto viz_publisher =
+    builder.template AddSystem<systems::DrakeVisualizer>(
+      rigidBodyPlant->get_rigid_body_tree(), &lcm);
+
+  std::unique_ptr<systems::Diagram<double>> diagram = builder.Build();
+  systems::Simulator<double> simulator(*diagram);
+  simulator.Initialize();
+  simulator.StepTo(0.1);
 }
 
 }
@@ -69,7 +157,7 @@ void PrintHelp() {
 int main (int argc, char **argv) {
   std::cout << "[sdf_sample]: Program started" << std::endl;
 
-  std::cout << "[sdf_sample]: Program invoked: ";
+  /*std::cout << "[sdf_sample]: Program invoked: ";
   for (int i = 0; i < argc; i++) {
     std::cout << argv[i] << " ";
   }
@@ -81,10 +169,11 @@ int main (int argc, char **argv) {
     exit(-1);
   }
   // Get the sdf file path
-  std::string sdfFilePath(reinterpret_cast< char const* >(argv[1]));
+  std::string sdfFilePath(reinterpret_cast< char const* >(argv[1]));*/
+  std::string sdfFilePath(drake::GetDrakePath() + "/sdf_sample/models/box.sdf");
   std::cout << "[sdf_sample]: SDF file path: " << sdfFilePath << std::endl;
   // Load the sdf file into memory
-  drake::sdf_sample::LoadSDFSample(sdfFilePath);
+  drake::sdf_sample::LoadSDFSample2(sdfFilePath);
 
   return 0;
 }
