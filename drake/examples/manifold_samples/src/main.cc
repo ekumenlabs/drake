410318bb04886f518f6ac7d0299a3afbb8fddb7f
Agustin Alba Chicar <ag.albachicar@gmail.com>
2017-02-16 17:50:14 +0000

    Program loading a sample RNDF file from Manifold and then printing their names.

--- drake/examples/manifold_samples/src/main.cc
+++ drake/examples/manifold_samples/src/main.cc
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <ignition/math.hh>
+#include <manifold/RoadNetwork.hh>
+#include <manifold/rndf/RNDF.hh>
 
 int main (int argc, char **argv) {
   ignition::math::Vector3d p(0.0, 1.0, 2.0);
   std::cout << "Hello world!" << std::endl;
   std::cout << "My vector is: " << p << std::endl;
+
+  manifold::rndf::RNDF info("/tmp/manifold/test/rndf/sample1.rndf");
+  manifold::RoadNetwork network(info);
+
+  for (auto vertex : network.Graph().Vertexes()) {
+    std::cout << vertex->Name() << std::endl;
+  }
+
   return 0;
 }
