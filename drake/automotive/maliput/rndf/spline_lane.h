3b98c82276267ce841bf1017d59f1c067b1d5ff7
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2017-04-05 15:44:41 -0300

    Added the implementation of a spline lane.

---
+++ drake/automotive/maliput/rndf/spline_lane.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cmath>
+
+#include "drake/automotive/maliput/rndf/lane.h"
+#include "drake/common/drake_assert.h"
+#include "drake/common/drake_copyable.h"
+
+#include <ignition/math.hh>
+
+namespace drake {
+namespace maliput {
+namespace rndf {
+
+class SplineLane : public Lane {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(SplineLane)
+
+  typedef Eigen::Matrix<double, 2, 1, Eigen::DontAlign> Point2;
+
+  SplineLane(const api::LaneId& id, const api::Segment* segment,
+          const std::vector<Point2> &control_points,
+          const double theta_i, const double theta_f,
+          const api::RBounds& lane_bounds,
+          const api::RBounds& driveable_bounds,
+          const CubicPolynomial& elevation,
+          const CubicPolynomial& superelevation);
+
+  ~SplineLane() override = default;
+
+  static Point2 IgnitionVector3d2Point2(
+  	const ignition::math::Vector3d &point);
+
+  static ignition::math::Vector3d Point22IgnitionVector3d(
+    const Point2 &point);
+
+  static std::vector<ignition::math::Vector3d> Points22IgnitionVector3ds(
+    const std::vector<Point2> &points);
+
+  static std::vector<Point2> IgnitionVector3ds2Points(
+  	const std::vector<ignition::math::Vector3d> &points);
+
+  static double ComputeLength(
+    const std::vector<ignition::math::Vector3d> &points,
+    std::vector<double> *lengths);
+
+  static std::vector<ignition::math::Vector3d> InterpolateRoad(
+    const std::vector<ignition::math::Vector3d> &_points,
+    const double distance_threshold);
+
+ private:
+  api::LanePosition DoToLanePosition(
+      const api::GeoPosition& geo_pos,
+      api::GeoPosition* nearest_point,
+      double* distance) const override;
+
+  V2 xy_of_p(const double p) const override;
+  double heading_of_p(const double p) const override;
+
+  std::vector<ignition::math::Vector3d> control_points_;
+  std::vector<double> lengths_;
+  ignition::math::Spline spline_;
+  double theta_i_;
+  double theta_f_;
+};
+
+}  // namespace rndf
+}  // namespace maliput
+}  // namespace drake
--- drake/automotive/rndf_generator.cc
+++ drake/automotive/rndf_generator.cc
@@ -19,30 +19,44 @@ RNDFTBuilder::Build() {
   const rndf::EndpointZ kFlatZ90{0., 0., 0., 0.};
   const rndf::Endpoint kRoadOrigin{kOriginXy, kFlatZ};
 
-  const double& kLength = 50.;
+  const double& kLength50 = 50.;
+  const double& kLength20 = 20.;
+  const double& kRadius10 = 10.;
+  const double& kRadius20 = 20.;
 
   const auto& s1l1 = rb->Connect(
   	"s1l1",
   	kRoadOrigin,
-  	kLength,
+  	kLength50,
   	kFlatZ);
   /*const auto& s2l1 = */rb->Connect(
   	"s2l1",
   	s1l1->end(),
-  	kLength,
+  	kLength50,
   	kFlatZ);
 
-  const double& kRadius = 10.;
   const auto& s1l1tos3l1 = rb->Connect(
   	"s1l1tos3l1",
   	s1l1->end(),
-  	rndf::ArcOffset(kRadius, M_PI/2.0),
+  	rndf::ArcOffset(kRadius10, M_PI/2.0),
   	kFlatZ);
 
-  /*const auto& s2l1 = */rb->Connect(
+  const auto& s3l1 = rb->Connect(
   	"s3l1",
   	s1l1tos3l1->end(),
-  	kLength,
+  	kLength50,
+  	kFlatZ);
+
+  const auto& s3l1tos4l1 = rb->Connect(
+  	"s3l1tos4l1",
+  	s3l1->end(),
+  	rndf::ArcOffset(kRadius20, -M_PI/2.0),
+  	kFlatZ);
+
+  /*const auto& s4l1 = */rb->Connect(
+  	"s4l1",
+  	s3l1tos4l1->end(),
+  	kLength20,
   	kFlatZ);
 
   return rb->Build({"rndf-T-example"});
