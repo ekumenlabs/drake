cb888a28c18cd9444f5454f5d2c3208b3076e366
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2017-04-05 17:08:38 -0300

    Joined the code of splines but got something similar to a line.

--- drake/automotive/rndf_generator.cc
+++ drake/automotive/rndf_generator.cc
@@ -22,7 +22,7 @@ RNDFTBuilder::Build() {
   const double& kLength50 = 50.;
   const double& kLength20 = 20.;
   const double& kRadius10 = 10.;
-  const double& kRadius20 = 20.;
+  //const double& kRadius20 = 20.;
 
   const auto& s1l1 = rb->Connect(
   	"s1l1",
@@ -47,11 +47,22 @@ RNDFTBuilder::Build() {
   	kLength50,
   	kFlatZ);
 
-  const auto& s3l1tos4l1 = rb->Connect(
+  /*const auto& s3l1tos4l1 = rb->Connect(
   	"s3l1tos4l1",
   	s3l1->end(),
   	rndf::ArcOffset(kRadius20, -M_PI/2.0),
-  	kFlatZ);
+  	kFlatZ);*/
+  std::vector<rndf::Endpoint> endpoints;
+  endpoints.push_back(s3l1->end());
+  endpoints.push_back(rndf::Endpoint(
+    rndf::EndpointXy(75.0, 75.0, 0.0),
+    kFlatZ));
+  endpoints.push_back(rndf::Endpoint(
+    rndf::EndpointXy(80.0, 80.0, 0.0),
+    kFlatZ));
+  const auto& s3l1tos4l1 = rb->Connect(
+    "s3l1tos4l1",
+    endpoints);
 
   /*const auto& s4l1 = */rb->Connect(
   	"s4l1",
