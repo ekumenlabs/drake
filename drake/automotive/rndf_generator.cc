81bf11355e6be2b93337573d842e65745f301d6d
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2017-04-06 16:56:46 -0300

    Added the path through arguments.

--- drake/automotive/rndf_generator.cc
+++ drake/automotive/rndf_generator.cc
@@ -19,92 +19,92 @@ RNDFTBuilder::Build() {
   const rndf::EndpointZ kFlatZ90{0., 0., 0., 0.};
   const rndf::Endpoint kRoadOrigin{kOriginXy, kFlatZ};
 
-  //const double& kLength50 = 50.;
-  //const double& kLength20 = 20.;
-  //const double& kRadius10 = 10.;
+  // //const double& kLength50 = 50.;
+  // //const double& kLength20 = 20.;
+  // //const double& kRadius10 = 10.;
 
   std::vector<rndf::Endpoint> endpoints;
 
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(0.0, 0.0, 0.0),
-    kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(50.0, 0.0, 0.0),
-    kFlatZ));
-  /*const auto& s1l1 =*/ rb->Connect(
-    "s1l1",
-    endpoints);
-  endpoints.clear();
-
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(50.0, 0.0, 0.0),
-    kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(100.0, 0.0, 0.0),
-    kFlatZ));
-  /*const auto& s2l1 = */rb->Connect(
-    "s2l1",
-    endpoints);
-  endpoints.clear();
-
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(50.0, 0.0, 0.0),
-    kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(60.0, 10.0, 0.0),
-    kFlatZ));
-  /*const auto& s1l1tos3l1 = */rb->Connect(
-    "s1l1tos3l1",
-    endpoints);
-  endpoints.clear();
-
-
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(60.0, 10.0, 0.0),
-    kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(60.0, 60.0, 0.0),
-    kFlatZ));
-  /*const auto& s3l1 = */rb->Connect(
-    "s3l1",
-    endpoints);
-  endpoints.clear();
-
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(60.0, 60.0, 0.0),
-    kFlatZ));
   // endpoints.push_back(rndf::Endpoint(
-  //   rndf::EndpointXy(61.801, 63.7987, 0.0),
+  //   rndf::EndpointXy(0.0, 0.0, 0.0),
   //   kFlatZ));
   // endpoints.push_back(rndf::Endpoint(
-  //   rndf::EndpointXy(65.5141, 69.6438, 0.0),
+  //   rndf::EndpointXy(50.0, 0.0, 0.0),
   //   kFlatZ));
-  // endpoints.push_back(rndf::Endpoint(
-  //   rndf::EndpointXy(72.9029, 75.2812, 0.0),
-  //   kFlatZ));
-  // endpoints.push_back(rndf::Endpoint(
-  //   rndf::EndpointXy(77.5213, 79.2248, 0.0),
-  //   kFlatZ));
-  // endpoints.push_back(rndf::Endpoint(
-  //   rndf::EndpointXy(79.6613, 79.9177, 0.0),
-  //   kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(80.0, 80.0, 0.0),
-    kFlatZ));
-  /*const auto& s3l1tos4l1 = */rb->Connect(
-    "s3l1tos4l1",
-    endpoints);
+  // /*const auto& s1l1 =*/ rb->Connect(
+  //   "s1l1",
+  //   endpoints);
+  // endpoints.clear();
 
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(80.0, 80.0, 0.0),
-    kFlatZ));
-  endpoints.push_back(rndf::Endpoint(
-    rndf::EndpointXy(100.0, 80.0, 0.0),
-    kFlatZ));
-  /*const auto& s4l1 = */rb->Connect(
-    "s4l1",
-    endpoints);
-  endpoints.clear();
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(50.0, 0.0, 0.0),
+  //   kFlatZ));
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(100.0, 0.0, 0.0),
+  //   kFlatZ));
+  // /*const auto& s2l1 = */rb->Connect(
+  //   "s2l1",
+  //   endpoints);
+  // endpoints.clear();
+
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(50.0, 0.0, 0.0),
+  //   kFlatZ));
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(60.0, 10.0, 0.0),
+  //   kFlatZ));
+  // /*const auto& s1l1tos3l1 = */rb->Connect(
+  //   "s1l1tos3l1",
+  //   endpoints);
+  // endpoints.clear();
+
+
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(60.0, 10.0, 0.0),
+  //   kFlatZ));
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(60.0, 60.0, 0.0),
+  //   kFlatZ));
+  // /*const auto& s3l1 = */rb->Connect(
+  //   "s3l1",
+  //   endpoints);
+  // endpoints.clear();
+
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(60.0, 60.0, 0.0),
+  //   kFlatZ));
+  // // endpoints.push_back(rndf::Endpoint(
+  // //   rndf::EndpointXy(61.801, 63.7987, 0.0),
+  // //   kFlatZ));
+  // // endpoints.push_back(rndf::Endpoint(
+  // //   rndf::EndpointXy(65.5141, 69.6438, 0.0),
+  // //   kFlatZ));
+  // // endpoints.push_back(rndf::Endpoint(
+  // //   rndf::EndpointXy(72.9029, 75.2812, 0.0),
+  // //   kFlatZ));
+  // // endpoints.push_back(rndf::Endpoint(
+  // //   rndf::EndpointXy(77.5213, 79.2248, 0.0),
+  // //   kFlatZ));
+  // // endpoints.push_back(rndf::Endpoint(
+  // //   rndf::EndpointXy(79.6613, 79.9177, 0.0),
+  // //   kFlatZ));
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(80.0, 80.0, 0.0),
+  //   kFlatZ));
+  // /*const auto& s3l1tos4l1 = */rb->Connect(
+  //   "s3l1tos4l1",
+  //   endpoints);
+
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(80.0, 80.0, 0.0),
+  //   kFlatZ));
+  // endpoints.push_back(rndf::Endpoint(
+  //   rndf::EndpointXy(100.0, 80.0, 0.0),
+  //   kFlatZ));
+  // /*const auto& s4l1 = */rb->Connect(
+  //   "s4l1",
+  //   endpoints);
+  // endpoints.clear();
 
   endpoints.push_back(rndf::Endpoint(
     rndf::EndpointXy(0.0, 0.0, 0.0),
