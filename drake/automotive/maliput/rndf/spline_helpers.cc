5bee2ee82cc839ad6e6cbef5bcb0783413294fc8
Michel Hidalgo <hid.michel@gmail.com>
2017-04-12 11:40:22 -0300

    WIP on arc length parameterizations for ignition::math::Spline

---
+++ drake/automotive/maliput/rndf/spline_helpers.cc
@@ -0,0 +1,111 @@
+#include <algorithm>
+
+#include "drake/automotive/maliput/rndf/spline_helpers.h"
+#include "drake/common/drake_assert.h"
+#include "drake/common/drake_throw.h"
+
+namespace drake {
+namespace maliput {
+namespace rndf {
+
+InverseArcLengthInterpolator::InverseArcLengthInterpolator(
+    const int _num_of_segments)    
+{
+  // Make sure the number of linear segments
+  // used for approximation is a positive integer.
+  DRAKE_THROW_UNLESS(_num_segments > 0);
+  // Add one extra space to hold the end point.
+  this->s_t_.resize(_num_of_segments + 1);
+  this->dt_ = 1.0 / _num_of_segments;
+}
+
+void InverseArcLengthInterpolator::Fit(const ignition::math::Spline &_spline)
+{
+  for (int i = 0 ; i < this->s_t_.size() ; ++i) {
+    this->s_t_[i] = _spline.ArcLength(this->dt_ * i);
+  }
+}
+
+double InverseArcLengthInterpolator::InterpolateMthDerivative(
+    const int _mth, const double _s) const
+{
+  // Make sure that the derivative order is a positive integer.
+  DRAKE_THROW_UNLESS(_mth >= 0);
+  // Make sure that the arc length requested is not below
+  // s(t) image interval low bound.
+  DRAKE_THROW_UNLESS(this->s_t_.front() <= _s);
+  // Make sure that the arc length requested is not below
+  // s(t) image interval upper bound.
+  DRAKE_THROW_UNLESS(this->s_t_.back() >= _s);
+
+  // Search s(t) function tabulation for lower bound index
+  auto it = std::lower_bound(this->s_t_.begin(), this->s_t_.end(), _s);
+  if (it != this->s_t_.begin()) it--;
+  int index = it - this->s_t_.begin();
+
+  // Zero out any derivative of higher order than
+  // 1, as this is a piecewise linear interpolant.
+  if (_mth > 1) return 0.0;
+  
+  double ds = this->s_t_[index + 1] - this->s_t_[index];
+
+  // Return t'(s) at _s.
+  if (_mth == 1) return this->dt_ / ds;
+  
+  double t_0 = index * this->dt_;
+  
+  // Return t(s) at _s.
+  return t_0 + (this->dt_ / ds) * (_s - this->s_t_[i]);
+}
+
+ArcLengthParameterizedSpline::ArcLengthParameterizedSpline(
+    const std::unique_ptr<Spline>& _spline, const int _num_of_segments)
+    : q_t_(std::move(_spline)), t_s(_num_of_segments)
+{
+  DRAKE_ASSERT(this->q_t_ != nullptr);
+  // Fit inverse arc length interpolator to the
+  // given spline arc length
+  t_s_.Fit(*this->q_t_);
+}
+
+ignition::math::Vector3d
+ArcLengthParameterizedSpline::InterpolateMthDerivative(
+    const int _mth, const double _s) const
+{
+  if (_mth > 3) {
+    // M > 3 => p = 0 (as this is a cubic interpolator)
+    return ignition::math::Vector3d(0.0, 0.0, 0.0);
+  }
+  double t_s = this->t_s_.InterpolateMthDerivative(0, _s);
+  if (_mth < 1) {
+    // M = 0 => P(s) = Q(t(s))
+    return this->q_t_.InterpolateMthDerivative(0, t_s);
+  }
+  double t_prime_s = this->t_s_.InterpolateMthDerivative(1, _s);
+  ignition::math::Vector3d q_prime_t = this->q_t_.InterpolateMthDerivative(1, t_s);
+  if (_mth < 2) {
+    // M = 1 => P'(s) = Q'(t(s)) * t'(s)
+    return q_prime_t * t_prime_s;
+  }
+  double t_prime_s_2 = t_prime_s * t_prime_s;
+  double t_prime2_s = this->t_s_.InterpolateMthDerivative(2, _s);
+  ignition::math::Vector3d q_prime2_t = this->q_t_.InterpolateMthDerivative(2, t_s);
+  if (_mth < 3) {
+    // M = 2 => P''(s) = Q''(t(s)) * t'(s)^2 + Q'(t(s)) * t''(s)
+    return q_prime2_t * t_prime_s_2 + q_prime_t * t_prime2_s;
+  }
+  double t_prime_s_3 = t_prime_s_2 * t_prime_s;
+  double t_prime3_s = this->t_s_.InterpolateMthDerivative(3, _s);
+  ignition::math::Vector3d q_prime3_t = this->q_t_.InterpolateMthDerivative(3, t_s);
+  // M = 3 => P'''(s) = Q'''(t(s)) * t'(s)^3
+  ///                   + 3 * Q''(t(s)) * t'(s) * t''(s)
+  ///                   + Q'(t(s)) * t'''(s)
+  return (q_prime3_t * t_prime_s_3
+          + 3 * q_prime2_t * t_prime_s * t_prime2_s
+          + q_prime_t * t_prime3_s);
+}
+
+
+}  // namespace rndf
+}  // namespace maliput
+}  // namespace drake
---
+++ drake/automotive/maliput/rndf/spline_helpers.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "ignition/math/Spline.hh"
+#include "ignition/math/Vector3.hh"
+
+#include "drake/common/drake_copyable.h"
+
+namespace drake {
+namespace maliput {
+namespace rndf {
+
+/// An interpolator for inverse arc length functions
+/// of arbitrary ignition::math::Splines. Helpful for arc length
+/// parameterization.
+class InverseArcLengthInterpolator {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ArcLengthParameterizedSpline)
+
+  /// Constructor that takes the number of segments to
+  /// use for piecewise interpolation.
+  /// @param[in] _num_of_segments a positive integer describing
+  /// the amount of segments to be used for piecewise interpolation
+  /// @throws whenever arguments constrains are not
+  /// satisfied.
+  explicit InverseArcLengthInterpolator(const int _num_of_segments);
+
+  /// Fits this interpolator to match the inverse of
+  /// the given spline arc length function @f$ s(t) @f$.
+  /// @param[in] _spline to interpolate for
+  void Fit(const ignition::math::Spline& _spline);
+
+  /// Interpolates @f$ t(s) @f$, that is, the inverse arc length
+  /// function fitted.
+  /// @param[in] _mth a positive plus 0 integer describing
+  /// the order of the function derivative to interpolate.
+  /// @param[in] _s arc length to interpolate at, constrained.
+  /// by the curve dimensions [0, arc_length].
+  /// @return interpolated mth derivative @f$ t^m(s) @f$ .
+  /// @throws whenever arguments constrains are not
+  /// satisfied.
+  double InterpolateMthDerivative(const int _mth,
+                                  const double _s) const;
+
+ private:
+  std::vector<double> s_t_; ///< Arc length function s(t) as a table.
+  double dt_; ///< Step value in the independent variable t.
+};
+
+/// An extension for ignition::math::Splines that reparameterizes
+/// them by arc length.
+class ArcLengthParameterizedSpline {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(ArcLengthParameterizedSpline)
+
+  /// Constructor that takes a spline and a number of segments
+  /// for arc length parameterization approximations.
+  /// @param[in] _spline to interpolate for.
+  /// @param[in] _num_of_segments a positive integer describing
+  /// the amount of segments to be used for piecewise interpolation.
+  /// @throws whenever arguments constrains are not satisfied.
+  explicit ArcLengthParameterizedSpline(const std::unique_ptr<Spline>& _spline,
+                                        const int _num_of_segments);
+
+  /// Interpolates @f$ Q(s) @f$, that is, the spline parameterized
+  /// by arc length.
+  /// @param[in] _mth a positive plus 0 integer describing
+  /// the order of the function derivative to interpolate.
+  /// @param[in] _s arc length to interpolate at, constrained.
+  /// by the curve dimensions [0, arc_length].
+  /// @return the mth derivative Q^m(s) 
+  /// @throws whenever arguments constrains are not satisfied.
+  ignition::math::Vector3d InterpolateMthDerivative(
+      const int _mth, const double _s) const;
+
+  /// Returns a mutable pointer to the underlying spline
+  inline ignition::math::Spline* BaseSpline() {
+    return this->spline_.get();
+  }
+  
+ private:
+  std::unique_ptr<ignition::math::Spline> q_t_; ///< Parameterized curve Q(t).
+  InverseArcLengthInterpolator t_s_; ///< Inverse arc length function t(s).
+};
+
+}  // namespace rndf
+}  // namespace maliput
+}  // namespace drake
