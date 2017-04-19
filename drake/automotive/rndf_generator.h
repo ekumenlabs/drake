1fb54cf6a72ed87b2dbb942110602b4c7e874839
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2017-04-19 10:23:27 -0300

    Updates on the code to detect the heading.

--- drake/automotive/rndf_generator.h
+++ drake/automotive/rndf_generator.h
@@ -208,7 +208,8 @@ class RNDFTBuilder {
 
   /// Tolerances for monolane's Builder.
   const double linear_tolerance_  = 0.01;
-  const double angular_tolerance_ = 2.0 * M_PI; /*0.01 * M_PI;*/
+  const double angular_tolerance_ = 2.0 * M_PI;
+  //const double angular_tolerance_ = 0.01 * M_PI;
 
   const RNDFRoadCharacteristics rc_;
 };
